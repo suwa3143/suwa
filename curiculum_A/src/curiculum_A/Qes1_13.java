489b80c288d0457058e9fc5b57267d9ae2910b9f
user <user@DESKTOP-7JT28GT>
2025-05-24 21:44:33 +0900

    課題Java1

---
+++ curiculum_A/src/curiculum_A/Qes1_13.java
@@ -0,0 +1,41 @@
+package curiculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		/**********************************************************************************
+         * [概要] データ型ごとのローカル変数宣言処理
+         * [詳細] 各データ型（byte, short, int, long, float, double, char, String, boolean）の
+         *        変数をローカルで宣言する
+         **********************************************************************************/
+
+        // バイト型の変数を宣言
+        byte byteVal;
+
+        // 短整数型の変数を宣言
+        short shortVal;
+
+        // 整数型の変数を宣言
+        int intVal;
+
+        // 長整数型の変数を宣言
+        long longVal;
+
+        // 単精度浮動小数点数型の変数を宣言
+        float floatVal;
+
+        // 倍精度浮動小数点数型の変数を宣言
+        double doubleVal;
+
+        // 文字型の変数を宣言
+        char charVal;
+
+        // 文字列型の変数を宣言
+        String stringVal;
+
+        // ブーリアン型の変数を宣言
+        boolean boolVal;
+
+	}
+
+}
