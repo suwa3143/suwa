439c9070eb5bd39abcff7d8d3c495604aad221a1
user <user@DESKTOP-7JT28GT>
2025-06-04 21:25:44 +0900

    課題1-13修正しました。

--- curiculum_A/src/curiculum_A/Qes1_13.java
+++ curiculum_A/src/curiculum_A/Qes1_13.java
@@ -8,18 +8,18 @@ package curiculum_A;
 public class Qes1_13 {
 
     public static void main(String[] args) {
-        課題1();
-        課題2();
-        課題34();
-        課題5();
-        課題6();
-        課題7();
-        課題8();
-        課題9();
-        課題10();
-        課題11();
-        課題12();
-        課題13();
+        assignment1();
+        assignment2();
+        assignment34();
+        assignment5();
+        assignment6();
+        assignment7();
+        assignment8();
+        assignment9();
+        assignment10();
+        assignment11();
+        assignment12();
+        assignment13();
     }
 
     /**************************************************************************
@@ -27,7 +27,7 @@ public class Qes1_13 {
      * [詳細]各データ型(byte,short,int,long,float,double,char,String,boolean)の
      *　　　 変数をローカルで宣言する 
      *************************************************************************/
-    public static void 課題1() {
+    public static void assignment1() {
         // バイト型の変数を宣言
         byte byteVal;
         
@@ -60,23 +60,23 @@ public class Qes1_13 {
      * [概要] 課題2：初期値代入・初期化
      * [詳細] 9個の変数に初期値を代入して初期化する
      **************************************************************************/
-    public static void 課題2() {
-        byte byteVal = 10;
-        short shortVal = 100;
-        int intVal = 1000;
-        long longVal = 10000L;
-        float floatVal = 9.5f;
-        double doubleVal = 10.5;
-        char charVal = 'a';
-        String stringVal = "ハロー";
-        boolean booleanVal = true;
+    public static void assignment2() {
+        byte byteVal = 0;
+        short shortVal = 0;
+        int intVal = 0;
+        long longVal = 0L;
+        float floatVal = 0.0f;
+        double doubleVal = 0.0d;
+        char charVal = '\u0000';
+        String stringVal = "null";
+        boolean booleanVal = false;
     }
 
     /**************************************************************************
      * [概要] 課題3,4：コンソール出力
      * [詳細] 指定の計算や出力を行う
      **************************************************************************/
-    public static void 課題34() {
+    public static void assignment34() {
         byte b = 10;
         short s = 100;
         int i = 1000;
@@ -113,7 +113,7 @@ public class Qes1_13 {
      * [概要]  課題5：文字列と数値の連結修正
      * [詳細]  文字列連結を数値計算に修正する
      **************************************************************************/
-    public static void 課題5() {
+    public static void assignment5() {
     	 String num = "20";
          int num1 = 23;
 
@@ -125,7 +125,7 @@ public class Qes1_13 {
      * [概要]  課題6：自己紹介情報変数に格納し出力
      * [詳細]  指定のフォーマットで出力する
      **************************************************************************/
-    public static void 課題6() {
+    public static void assignment6() {
     	 String name = "山田太郎";
          int age = 18;
          double height = 170.5;
@@ -143,7 +143,7 @@ public class Qes1_13 {
      * [概要] 課題7：BMI計算と出力
      * [詳細] BMIを変数を使って計算し出力する
      **************************************************************************/
-    public static void 課題7() {
+    public static void assignment7() {
          double height = 170.5;
          double weight = 62.2;
 
@@ -157,7 +157,7 @@ public class Qes1_13 {
      * [概要] 課題8：変数に再代入し自己紹介を出力
      * [詳細] 新しい値に変数を再代入して出力する
      **************************************************************************/
-    public static void 課題8() {
+    public static void assignment8() {
     	String name = "鈴木一郎";
         int age = 24;
         double height = 168.5;
@@ -179,7 +179,7 @@ public class Qes1_13 {
      * [概要] 課題9：数値和算の自己代入と出力
      * [詳細] 変数の数値部分を自己代入し出力
      *************************************************************************-*/
-    public static void 課題9() {
+    public static void assignment9() {
     	String name = "鈴木一郎";
         int age = 24;
         double height = 168.5;
@@ -206,7 +206,7 @@ public class Qes1_13 {
      * [概要] 課題10：年齢判定（if文なし）
      * [詳細] 年齢が25以上ならtrueを出力する（if文使わず）
      **************************************************************************/
-    public static void 課題10() {
+    public static void assignment10() {
     	 int age = 24;
          System.out.println(age >= 25);
     }
@@ -215,7 +215,7 @@ public class Qes1_13 {
      * [概要] 課題11：年齢・身長・体重を文字列に変換して連結し出力
      * [詳細] それぞれ文字列に変換しつなげて出力する
      **************************************************************************/
-    public static void 課題11() {
+    public static void assignment11() {
     	 int age = 24;
          double height = 168.5;
          double weight = 64.2;
@@ -228,7 +228,7 @@ public class Qes1_13 {
      * [概要] 課題12：文字列から整数型に変換して出力
      * [詳細] 文字列を整数に変換し出力する
      **************************************************************************/
-    public static void 課題12() {
+    public static void assignment12() {
     	String ageStr = "24";
         String heightStr = "168";
 
@@ -243,7 +243,7 @@ public class Qes1_13 {
      * [概要] 課題13：条件判定の論理演算出力（if文禁止）
      * [詳細] 年齢が25以上または身長が160以上の条件判定を行い出力
      **************************************************************************/
-    public static void 課題13() {
+    public static void assignment13() {
         int age = 48;
         int height = 337;
 
